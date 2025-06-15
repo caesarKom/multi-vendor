a0aa52001fb64db267e4055039e90ce8c0d3b691
caesarKom <cezarykomorowski@gmail.com>
2025-06-15 22:26:47 +0200

    trpc integracion fix

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -2,12 +2,15 @@ import { initTRPC } from "@trpc/server"
 import { getPayload } from "payload"
 import config from "@payload-config"
 import { cache } from "react"
+import superison from "superjson"
 
 export const createTRPCContext = cache(async () => {
   return { userId: "user_123" }
 })
 
-const t = initTRPC.create({})
+const t = initTRPC.create({
+  transformer: superison,
+})
 
 export const createTRPCRouter = t.router
 export const createCallerFactory = t.createCallerFactory
