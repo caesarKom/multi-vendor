6cbc8d5ebc53340dedc5a101e51cdfc64e45becb
caesarKom <cezarykomorowski@gmail.com>
2025-06-16 21:19:04 +0200

    category-page

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 import { getQueryClient, trpc } from "@/trpc/server"
-import { SearchFilters, SearchFiltersSkeleton } from "./search-filters"
-import Footer from "./footer"
-import Navbar from "./navbar"
-import { Suspense } from "react"
+import {
+  SearchFilters,
+  SearchFiltersSkeleton,
+} from "@/modules/home/ui/components/search-filters"
+import Footer from "@/modules/home/ui/components/footer"
+import Navbar from "@/modules/home/ui/components/navbar"
 
 interface Props {
   children: React.ReactNode
