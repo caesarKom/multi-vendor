5a184e125b0bbaa4742e9844c259e9f36ee985b1
caesarKom <cezarykomorowski@gmail.com>
2025-06-23 23:17:49 +0200

    fix media

--- src/modules/products/ui/components/cart-button.tsx
+++ src/modules/products/ui/components/cart-button.tsx
@@ -19,7 +19,10 @@ export const CartButton = ({ tenantSlug, productId, isPurchased }: Props) => {
         asChild
         className="flex-1 font-medium bg-white h-12"
       >
-        <Link prefetch href={`/library/${productId}`}>
+        <Link
+          prefetch
+          href={`${process.env.NEXT_PUBLIC_APP_URL}/library/${productId}`}
+        >
           View in Library
         </Link>
       </Button>
